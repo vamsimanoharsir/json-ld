fc95c6125ecce63d2632b4df5c81753b39610ce4
VamsiManoharK <SKOMMA1@ford.com>
2025-02-16 13:56:40 +0530

    feat: implement sample json ld

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,8 @@
-ise<Metadata> {
+import type { Metadata } from "next";
+
+const baseUrl = "https://www.danmugh.com/";
+
+export async function generateMetadata(): Promise<Metadata> {
   const title = "My Blog";
 
   const description =
@@ -8,7 +12,23 @@ ise<Metadata> {
     metadataBase: new URL(baseUrl),
     title,
     description,
-    themeColor: "black"
+    themeColor: "black",
+    openGraph: {
+      title,
+      description,
+      url: baseUrl,
+      images: [
+        {
+          url: "ograph/public/NIR_4006.JPG",
+          secureUrl: "ograph/public/NIR_4006.JPG",
+          width: 1200,
+          height: 630,
+          alt: "Preview image for Vamsi's Blog",
+        },
+      ],
+      type: "website",
+      siteName: "Vamsi Blog",
+    },
   };
 }
 
@@ -19,13 +39,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${poppins.className} ${poppins.variable} font-sans bg-dark`}
-      >
+      <body>
         <div className="w-full min-h-screen flex flex-col sm:px-[100px] md:px-[140px] lg:px-[200px] xl:px-[225px]">
           {children}
         </div>
       </body>
     </html>
   );
-};
+}
---
+++ src/components/Contact.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Contact = () => {
+  return (
+    <section>
+      <h1>Contact</h1>
+      <p>Email: john.doe@example.com</p>
+      <p>Phone: 123-456-7890</p>
+    </section>
+  );
+};
+
+export default Contact;
